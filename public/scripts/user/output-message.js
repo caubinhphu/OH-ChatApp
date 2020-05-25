c6d4cf3a271a3ddb514ffdfff24b07e5fec3d29b
Hai tran do <10a3nguyenanhhai@gmail.com>
2020-05-25 15:04:00 +0700

    use notifyjs to output message

---
+++ public/scripts/user/output-message.js
@@ -0,0 +1,19 @@
+// output error message
+function outputErrorMessage(msg) {
+  $.notify(msg, 'error');
+}
+
+// output success message
+function outputSuccessMessage(msg) {
+  $.notify(msg, 'success');
+}
+
+// output warning message
+function outputWarnMessage(msg) {
+  $.notify(msg, 'warn');
+}
+
+// output info message
+function outputInfoMessage(msg) {
+  $.notify(msg, 'info');
+}
