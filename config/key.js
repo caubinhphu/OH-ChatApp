bac387420e61e32d710472560d1086b2a189b5d1
anhhai <10a3nguyenanhhai@gmail.com>
2021-05-13 20:49:23 +0700

    change key

--- config/key.js
+++ config/key.js
@@ -1,4 +1,4 @@
 module.exports = {
-  // host: 'https://oh-chatapp.herokuapp.com',
-  host: 'http://localhost:3000',
+  host: 'https://oh-chatapp.herokuapp.com',
+  // host: 'http://localhost:3000',
 };
